56084958bac10116b8b0580e60a0f92b1a59aa7a
robzard <pal-vitek1111@mail.ru>
2023-05-02 20:46:28 +0500

    new bot

--- main.py
+++ main.py
@@ -1,18 +1,86 @@
-import aiogram
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor, callback_data
+# -*- coding: utf-8 -*-
+import logging
 import sys
 
+import aiogram.types
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.types import MediaGroup, ContentType
+from aiogram.utils import executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import filters
+from aiogram.dispatcher.filters import Command
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
-
-TOKEN = sys.argv[1]
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+from aiogram.dispatcher.handler import CancelHandler
 
 invite_callback = CallbackData("invite", "action", "group")
 send_callback = CallbackData("invite", "action", "group")
 group_callback = CallbackData("invite", "action", "group")
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = sys.argv[1]
+bot = Bot(token=TOKEN)
+
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+send_button = KeyboardButton('Отправить', callback_data='send')
+cancel_button = KeyboardButton('Отмена', callback_data='cancel')
+markup_send = ReplyKeyboardMarkup(resize_keyboard=True).row(send_button, cancel_button)
+
+send_message_button = KeyboardButton('Отправить сообщение', callback_data='send')
+create_group_button = KeyboardButton('Создать группу чатов', callback_data='send')
+delete_group_button = KeyboardButton('Удалить группу чатов', callback_data='cancel')
+markup_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(send_message_button).row(create_group_button,
+                                                                                     delete_group_button)
+
+
+class MediaStates(StatesGroup):
+    waiting_for_media = State()
+
+
+class CreateGroupChat(StatesGroup):
+    create_group_chat = State()
+
+
+class DeleteGroupChat(StatesGroup):
+    delete_group_chat = State()
+
+
+class SelectGroup(StatesGroup):
+    select_group = State()
+
+
+class MyMiddleware(BaseMiddleware):
+    async def on_process_message(self, message: types.Message, data: dict):
+        if 'command' in data.keys():
+            if 'admin_teyla' in data['command'].text:
+                return
+        if not await have_id(message.from_user.id.__str__(), 'users'):
+            raise CancelHandler()
+
+
+async def get_groups_list():
+    with open("groups.txt", "r") as f:
+        lines = f.readlines()
+    list_groups = [el.replace('\n', '') for el in lines]
+    groups = list(dict.fromkeys(list_groups))
+    return groups
+
+async def get_chats_from_group(group):
+    groups = []
+    for chat_id in await get_all_chats():
+        if chat_id:
+            if group in chat_id:
+                group_id = int(chat_id.split(';')[0])
+                groups.append(group_id)
+    return groups
 
 
 async def write_id(id, filename):
@@ -44,6 +112,8 @@ async def get_all_chats():
         f_text = f.read()
     if f_text:
         return f_text.split('\n')
+    else:
+        return []
 
 
 async def delete_id_in_file(chat_id, file_name='chat'):
@@ -52,27 +122,41 @@ async def delete_id_in_file(chat_id, file_name='chat'):
         lines = f.readlines()
     with open(f"{file_name}.txt", "w") as f:
         for line in lines:
+            if chat_id in line.strip("\n"):
+                delete = True
             if chat_id not in line.strip("\n"):
                 f.write(line)
-            delete = True
     if delete:
         return True
     return False
 
 
-async def get_inline_buttons():
+async def get_inline_buttons(album=None):
+    album = [el.photo[-1].file_id for el in album].__str__()
+    if album is None:
+        album = []
     await create_file_if_not_exists('chats.txt')
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     with open("chats.txt", "r") as f:
         lines = f.readlines()
     list_groups = [el.split(';')[1] for el in lines if ';' in el]
     groups = list(dict.fromkeys(list_groups))
     for group in groups:
-        keyboard.add(types.InlineKeyboardButton(group, callback_data=invite_callback.new(action='select_group',
-                                                                                         group=group.replace('\n',
-                                                                                                             ''))))
-    keyboard.add(types.InlineKeyboardButton('Отправить всем',
-                                            callback_data=invite_callback.new(action='select_group', group='')))
+        keyboard.add(types.KeyboardButton(group))
+    keyboard.add(types.InlineKeyboardButton('Отправить всем'))
+    return keyboard
+
+
+async def get_keyboard_buttons_delete_groups():
+    await create_file_if_not_exists('groups.txt')
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    with open("groups.txt", "r") as f:
+        lines = f.readlines()
+    list_groups = [el for el in lines]
+    groups = list(dict.fromkeys(list_groups))
+    for group in groups:
+        keyboard.add(types.KeyboardButton(group))
+    keyboard.row(types.KeyboardButton('Назад в меню'))
     return keyboard
 
 
@@ -99,21 +183,21 @@ async def get_group_chat_name(text):
 
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
-        types.BotCommand("start", "Запустить бота"),
         types.BotCommand("add", "Добавить чат"),
         types.BotCommand("delete", "Удалить чат из рассылки"),
-        types.BotCommand("create_group", "Создать новую группу чатов"),
-        types.BotCommand("delete_group", "Удалить группу чатов"),
     ])
 
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
+async def get_menu_keyboard():
+    return ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Назад в меню'))
+
+
+@dp.message_handler(Command('start'))
+async def send_welcome(message: types.Message):
     await set_default_commands(dp)
-    print(message)
     if 'admin_teyla' in message.text:
         await write_id(message.from_user.id.__str__(), 'users')
-        await message.answer("Привет!\nОтправь мне любое сообщение и я отправлю его во все чаты!")
+        await message.answer("Привет!\nВыбери действие.", reply_markup=markup_menu)
 
 
 @dp.message_handler(commands=['add'])
@@ -121,6 +205,15 @@ async def process_start_command(message: types.Message):
     if await have_id(message.from_user.id.__str__(), 'users'):
         await message.reply("Выберите группу для добавления",
                             reply_markup=await get_inline_buttons_delete_groups('add_chat'))
+
+
+@dp.message_handler(commands=['delete'])
+async def process_start_command(message: types.Message):
+    if await have_id(message.from_user.id.__str__(), 'users'):
+        if await delete_id_in_file(message.chat.id.__str__(), 'chats'):
+            await message.answer("Чат удалён!")
+        else:
+            await message.answer("Такого чата нет в списке рассылок.")
 
 
 @dp.callback_query_handler(invite_callback.filter(action=["add_chat"]))
@@ -133,129 +226,140 @@ async def send_random_value(call: types.CallbackQuery, callback_data: dict):
             await call.message.answer("Чат уже был добавлен в рассылку!")
 
 
-@dp.message_handler(commands=['delete'])
-async def process_start_command(message: types.Message):
-    if await have_id(message.from_user.id.__str__(), 'users'):
-        if await delete_id_in_file(message.chat.id.__str__(), 'chats'):
-            await message.answer("Чат удалён!")
-        else:
-            await message.answer("Такого чата нет в списке рассылок.")
+@dp.message_handler(lambda message: message.text == "Назад в меню",
+                    state=[DeleteGroupChat.delete_group_chat,
+                           DeleteGroupChat.delete_group_chat,
+                           CreateGroupChat.create_group_chat,
+                           MediaStates.waiting_for_media,
+                           SelectGroup.select_group])
+async def send_welcomes1(message: types.Message, state: FSMContext):
+    await message.delete()
+    await message.answer("Отмена действия - возвращение в меню", reply_markup=markup_menu)
+    await state.finish()
 
 
-@dp.message_handler(commands=['create_group'])
-async def process_start_command(message: types.Message):
-    if await have_id(message.from_user.id.__str__(), 'users'):
-        name_group = await get_group_chat_name(message.text.replace('\n', ''))
-        if await write_id(name_group, 'groups'):
-            await bot.send_message(message.chat.id, f'Группа {name_group} добавлена!')
-        else:
-            await bot.send_message(message.chat.id, f'Группа {name_group} уже присутствует.')
+@dp.message_handler(lambda message: message.text == "Отправить сообщение")
+async def send_welcomes(message: types.Message):
+    await message.delete()
+    await message.answer("Выберите группу", reply_markup=await get_keyboard_buttons_delete_groups())
+    await SelectGroup.select_group.set()
 
 
-@dp.message_handler(commands=['delete_group'])  # ДОДЕЛАТЬ
-async def process_start_command(message: types.Message):
-    if await have_id(message.from_user.id.__str__(), 'users'):
-        if not message.group_chat_created:
-            await message.reply("Выберите группу для удаления",
-                                reply_markup=await get_inline_buttons_delete_groups())
-
-
-@dp.callback_query_handler(invite_callback.filter(action=["select_group"]))
-async def send_random_value(call: types.CallbackQuery, callback_data: dict):
-    keyboard = types.InlineKeyboardMarkup()
-    button_1 = types.InlineKeyboardButton(text="Отправить",
-                                          callback_data=send_callback.new(
-                                              action='send',
-                                              group=callback_data['group'].replace('\n', ''))
-                                          )
-    button_2 = types.InlineKeyboardButton(text="Не отправлять", callback_data='no_send')
-    keyboard.add(button_1).add(button_2)
-    # await call.message.reply("Отправить сообщение во все чаты?", reply_markup=keyboard)
-    await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=keyboard)
-
-
-@dp.callback_query_handler(invite_callback.filter(action=["delete_group"]))
-async def send_random_value(call: types.CallbackQuery, callback_data: dict):
-    keyboard = types.InlineKeyboardMarkup()
-    button_1 = types.InlineKeyboardButton(text="Удалить",
-                                          callback_data=send_callback.new(
-                                              action='delete_group_in_files',
-                                              group=callback_data['group'].replace('\n', ''))
-                                          )
-    button_2 = types.InlineKeyboardButton(text="Не удалять", callback_data='no_delete')
-    keyboard.add(button_1).add(button_2)
-    # await call.message.reply("Отправить сообщение во все чаты?", reply_markup=keyboard)
-    await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=keyboard)
-
-
-@dp.callback_query_handler(invite_callback.filter(action=["send"]))
-async def send_random_value(call: types.CallbackQuery, callback_data: dict):
-    count_send_chats = 0
-    for chat_id in await get_all_chats():
-        if chat_id:
-            if callback_data['group'] in chat_id:
-                count_send_chats += 1
-                group_id = int(chat_id.split(';')[0])
-                await call.message.reply_to_message.send_copy(group_id)
-
-                # try:
-                #     await call.message.reply_to_message.send_copy(group_id)
-                # except Exception as e:
-                #     err = 'The group has been migrated to a supergroup. New id: '
-                #     ex = str(e)
-                #     if err in ex:
-                #         new_id = ex.replace(err, '').replace('.', '').strip()
-                #         print(new_id)
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    await call.message.reply_to_message.reply(
-        f'Сообщение отправлено в группу "{callback_data["group"]}".\nКоличество чатов, в которые были направлены сообщения: {count_send_chats}')
-
-
-@dp.callback_query_handler(invite_callback.filter(action=["delete_group_in_files"]))
-async def send_random_value(call: types.CallbackQuery, callback_data: dict):
-    if await delete_id_in_file(callback_data['group'], 'groups'):
-        await delete_id_in_file(callback_data['group'], 'chats')
-        await bot.delete_message(call.message.chat.id, call.message.message_id)
-        await call.message.reply_to_message.reply(f"Группа {callback_data['group']} удалена!")
+@dp.message_handler(state=SelectGroup.select_group)
+async def send_welcomes(message: types.Message, state: FSMContext):
+    group = message.text
+    if message.text in await get_groups_list():
+        await message.answer(f'Отправьте сообщения, которые необходимо переслать в группу "{group}"',
+                             reply_markup=await get_menu_keyboard())
     else:
-        await call.message.reply_to_message.reply(f"Группа для удаления {callback_data['group']} не найдена.")
+        await message.answer(f'Группа "{message.text}" не найдена!', reply_markup=markup_menu)
+        await state.finish()
+        raise CancelHandler
+
+    await state.finish()
+    current_media = await MediaStates.waiting_for_media.set()
+    current_media = await Dispatcher.get_current().current_state().get_data()
+    current_media = current_media.get('group', [])
+    current_media.append(message.text)
+    await state.update_data(group=current_media)
 
 
-@dp.callback_query_handler(text='no_send')
-async def send_random_value(call: types.CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    await call.message.reply_to_message.reply("Сообщение не будет отправлено")
+@dp.message_handler(lambda message: message.text == "Создать группу чатов")
+async def send_welcomes(message: types.Message):
+    await message.delete()
+    await message.answer('Напишите название группы:', reply_markup=await get_menu_keyboard())
+    await CreateGroupChat.create_group_chat.set()
 
 
-@dp.callback_query_handler(text='no_delete')
-async def send_random_value(call: types.CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    await call.message.reply_to_message.reply("Сообщение не будет удалено")
+@dp.message_handler(state=CreateGroupChat.create_group_chat)
+async def send_welcomes(message: types.Message, state: FSMContext):
+    name_group = message.text
+    if await write_id(name_group, 'groups'):
+        await bot.send_message(message.chat.id, f'Группа {name_group} добавлена!', reply_markup=markup_menu)
+    else:
+        await bot.send_message(message.chat.id, f'Группа {name_group} уже присутствует.', reply_markup=markup_menu)
+    await state.finish()
 
 
-@dp.message_handler(content_types=aiogram.types.ContentType.all())
-async def process_start_command(message: types.__all__):
-    if message.chat.type == 'private':
-        if await have_id(message.from_user.id.__str__(), 'users'):
-            if not message.group_chat_created:
-                await message.reply("Выберите группу", reply_markup=await get_inline_buttons())
+@dp.message_handler(lambda message: message.text == "Удалить группу чатов")
+async def send_welcomes(message: types.Message):
+    await message.delete()
+    if not message.group_chat_created:
+        await message.answer("Выберите группу для удаления",
+                             reply_markup=await get_keyboard_buttons_delete_groups())
+    await DeleteGroupChat.delete_group_chat.set()
 
 
-ADMINS = [601610220]
+@dp.message_handler(state=DeleteGroupChat.delete_group_chat)
+async def send_welcomes(message: types.Message, state: FSMContext):
+    group = message.text
+    if await delete_id_in_file(group, 'groups'):
+        await delete_id_in_file(group, 'chats')
+        await message.answer(f"Группа {group} удалена!", reply_markup=markup_menu)
+    else:
+        await message.answer(f"Группа для удаления {group} не найдена.", reply_markup=markup_menu)
+    await state.finish()
 
 
-async def on_startup(db):
-    print('-START-')
-    for admin in ADMINS:
-        await bot.send_message(chat_id=admin, text="Чат-бот запущен.")
+@dp.message_handler(lambda message: message.text == "Отправить", state=MediaStates.waiting_for_media)
+async def send_welcomes(message: types.Message, state: FSMContext):
+    await message.delete()
+    data = await state.get_data()
+    media_group = MediaGroup()
+    media_group_docs = MediaGroup()
+    groups = await get_chats_from_group(data['group'])
+    if not groups:
+        await message.answer(f'В группе "{data["group"]}" отсутствуют добавленые чаты.', reply_markup=markup_menu)
+        await state.finish()
+        raise CancelHandler
+    for m in data['media']:
+        if m['type'] in ('photo', 'video'):
+            media_group.attach(m)
+        elif m['type'] == 'document':
+            media_group_docs.attach(m)
+        elif m['type'] == 'caption':
+            for group in groups:
+                await bot.send_message(chat_id=group, text=m['media'])
+    if media_group.media:
+        for group in groups:
+            await bot.send_media_group(chat_id=group, media=media_group)
+    if media_group_docs.media:
+        for group in groups:
+            await bot.send_media_group(chat_id=group, media=media_group_docs)
+    await state.finish()
+    await message.answer(f'Сообщение отправлено в группу "{data["group"]}".\nКоличество чатов, в которые были направлены сообщения: {len(groups)}', reply_markup=markup_menu)
 
 
-async def on_shutdown(db):
-    print('-END-')
-    for admin in ADMINS:
-        await bot.send_message(chat_id=admin, text="Я отключён!")
-    await bot.close()
+@dp.message_handler(lambda message: message.text == "Отмена", state=MediaStates.waiting_for_media)
+async def send_welcomes(message: types.Message, state: FSMContext):
+    await message.delete()
+    await state.finish()
+    await message.answer('Отправка отменена', reply_markup=markup_menu)
+
+
+@dp.message_handler(content_types=[ContentType.PHOTO, ContentType.VIDEO, ContentType.DOCUMENT, ContentType.TEXT],
+                    state=MediaStates.waiting_for_media)
+async def handle_media(message: types.Message, state: FSMContext):
+    current_media = await state.get_data()
+    group = current_media['group'][0]
+    current_media = current_media.get('media', [])
+
+    if message.text:
+        current_media.append({"media": message.text, "type": "caption"})
+
+    if message.photo:
+        current_media.append({"media": message.photo[-1].file_id, "type": "photo"})
+    elif message.video:
+        current_media.append({"media": message.video.file_id, "type": "video"})
+    elif message.document:
+        current_media.append({"media": message.document.file_id, "type": "document"})
+    if message.caption:
+        current_media.append({"media": message.caption, "type": "caption"})
+
+    await state.update_data(media=current_media, group=group)
+    await message.reply(f"Добавлено в сообщение для рассылки", reply_markup=markup_send)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup)
+    dp.middleware.setup(MyMiddleware())
+    executor.start_polling(dp, skip_updates=True)
